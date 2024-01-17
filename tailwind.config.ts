8138ff2c8904d1c1d0cad795f8d96e95b4a84e27
palmeiroerick <148393698+palmeiroerick@users.noreply.github.com>
2024-01-16 23:51:25 -0300

    chore(tailwind): correct tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,16 +4,14 @@ const config: Config = {
   content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
     screens: {
-      sm: "375px",
-      lg: "1440px",
-    },
+      sm: "375px",    },
     colors: {
       /* Primary */
       cyan: "#00fff7",
       softBlue: "#8bacda",
 
       /* Neutral */
-      white: "#000000",
+      white: "#ffffff",
       darkblueMain: "#0d192b",
       darkblueCard: "#14253d",
       darkblueLine: "#2f415b",
